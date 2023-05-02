5ec91a42b739190289ba7081e6dd7b8737199520
ufolx86 <victor.r.urueta@gmail.com>
2023-05-02 11:59:38 -0600

    Add files via upload

---
+++ MailSent.php
@@ -0,0 +1,31 @@
+      <?php
+         $to = "contacto@ruasabienesraicesqro.com";
+         $subject = "This is subject";
+         
+         $message = "<b>This is HTML message.</b>";
+         $message .= "<h1>This is headline.</h1>";
+         
+         $header = "From:victor.r.urueta@gmail.com \r\n";
+         $header .= "Cc:afgh@somedomain.com \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         
+         $retval = mail ($to,$subject,$message,$header);
+         
+         if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
+      ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
